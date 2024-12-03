418ef098e6da3f1f219f1bef8f5e7d7f0d199cc7
David <ledadadudede@gmail.com>
2024-12-03 10:49:56 +0100

    Update index.html : - Début de la NavBar Fix Routing : - Amélioration du Routage Docs README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Portfolio_Luka
+
+This portfolio was made with TailwindCSS.
+
+Icons are from [fontAwesome.com](https://fontAwesome.com)
+
+Luka Baudrant is a young IT student, freelancer, tutor and junior full-stack developer.
+
+He has already worked on various projects such as ‘Twins Rental’, ‘VLD Group’ and ‘SCO2’.
+
+This portfolio was created by David Baldo.
+
+## Good to know
+
+To run TailwindCSS : ```npm run dev```
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'darkBlue': {
+        800:'#6497b1',
+        900:'#03396c',
+      },
+      'marineBlue': {
+          100: '#eaf6ff',
+          200: '#b3cde0',
+          300: '#6497b1',
+          400: '#005b96',
+          500: '#03396c',
+          600: '#011f4b',
+      },
+    },
+    fontFamily: {
+      'Titles':['Raleway'],
+      'Body' : ['Mulish']
+    }
   },
   plugins: [],
 }
-
+}
